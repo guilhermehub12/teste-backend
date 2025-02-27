10b43bf16368c5eef881e993f95ba30ab5eda98f
Guilherme Delmiro <guilhermedelmiro11@gmail.com>
2025-02-27 18:55:01 -0300

    🔧chore: configuring App Services

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,12 +3,20 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
+use Opcodes\LogViewer\Facades\LogViewer;
+use Override;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
+    #[Override]
     public function register(): void
     {
         //
@@ -19,6 +27,62 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->setupLogViewer();
+        $this->configModels();
+        $this->configCommands();
+        $this->configUrls();
+        $this->configDate();
+    }
+
+     /**
+     * Sets up the LogViewer authentication to restrict access
+     * based on whether the authenticated user is an admin.
+     */
+    private function setupLogViewer(): void
+    {
+        LogViewer::auth(fn ($request) => $request->user()?->is_admin);
+    }
+
+    /**
+     * Configures Eloquent models by disabling the requirement for defining
+     * the fillable property and enforcing strict checking to ensure that
+     * all accessed properties exist within the model.
+     */
+    private function configModels(): void
+    {
+        // --
+        // Remove the need of the property fillable on each model
+        Model::unguard();
+
+        // --
+        // Make sure that all properties being called exists in the model
+        Model::shouldBeStrict();
+    }
+
+    /**
+     * Configures database commands to prohibit execution of destructive statements
+     * when the application is running in a production environment.
+     */
+    private function configCommands(): void
+    {
+        DB::prohibitDestructiveCommands(
+            app()->isProduction()
+        );
+    }
+
+    /**
+     * Configures the application URLs to enforce HTTPS protocol for all routes.
+     */
+    private function configUrls(): void
+    {
+        URL::forceHttps();
+    }
+
+    /**
+     * Configures the application to use CarbonImmutable for date and time handling.
+     */
+    private function configDate(): void
+    {
+        Date::use(CarbonImmutable::class);
     }
 }
