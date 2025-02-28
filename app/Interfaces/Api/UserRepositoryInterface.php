2cb347334a01f706d88918685ba803820285a7ab
Guilherme Delmiro <guilhermedelmiro11@gmail.com>
2025-02-28 16:33:36 -0300

    ✨feat: creating and configuring user interface

---
+++ app/Interfaces/Api/UserRepositoryInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Interfaces\Api;
+
+use Illuminate\Http\Request;
+
+interface UserRepositoryInterface
+{
+    public function getAll(Request $request);
+    public function create(array $data);
+    public function update(array $data, $id);
+    public function destroy($id);
+    public function findById($id);
+}
