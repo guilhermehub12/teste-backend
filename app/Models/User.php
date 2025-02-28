be65cf6a9ef44ce876bee9cbbc1fe6947397697e
Guilherme Delmiro <guilhermedelmiro11@gmail.com>
2025-02-28 15:44:53 -0300

    🗑️build: removing sanctum dependencies

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,13 +8,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 use OwenIt\Auditing\Contracts\Auditable;
 
 class User extends Authenticatable implements Auditable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasApiTokens;
     use HasFactory;
     use Notifiable;
     use \OwenIt\Auditing\Auditable;
