d77a578fa43d65a91d9254920ffd70a18b1b4d97
Guilherme Delmiro <guilhermedelmiro11@gmail.com>
2025-02-28 22:32:31 -0300

    ✨feat: creating and configuring request file to User Requests

---
+++ app/Http/Requests/Api/User/BaseUserRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Api\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class BaseUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'Nome',
+            'email' => 'E-mail',
+            'password' => 'Senha',
+            'telefone' => 'Telefone',
+            'is_admin' => 'Administrador',
+            'is_valid' => 'Ativo'
+        ];
+    }
+}
