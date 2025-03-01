4fd251ea1466dfaba274abd8cf54ded9dedba839
Guilherme Delmiro <guilhermedelmiro11@gmail.com>
2025-03-01 01:01:47 -0300

    feat: create user repository

---
+++ app/Repositories/Api/UserRepository.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Repositories\Api;
+
+use App\Interfaces\Api\UserRepositoryInterface;
+use App\Models\User;
+use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserRepository implements UserRepositoryInterface
+{
+    public function getAll(Request $request)
+    {
+        $perPage = $request->get('per_page', 5);
+        try {
+            return User::paginate($perPage);
+        } catch (Exception $ex) {
+            return false;
+        }
+    }
+
+    public function create(array $data)
+    {
+        try {
+            $user = User::create($data);
+            return $user;
+        } catch (Exception $ex) {
+            throw $ex;
+        }
+    }
+
+    public function update(array $data, $id)
+    {
+        try {
+            $user = User::findOrFail($id);
+            $user->update($data);
+            return $user;
+        } catch (ModelNotFoundException $ex) {
+            return false;
+        }
+    }
+
+    public function destroy($id)
+    {
+        try {
+            $user = User::findOrFail($id);
+            $user->delete();
+            return response()->noContent();
+        } catch (ModelNotFoundException $ex) {
+            return false;
+        }
+
+
+    }
+
+    public function findById($id)
+    {
+        try {
+            $user = User::findOrFail($id);
+        } catch (ModelNotFoundException $ex) {
+            return false;
+        }
+        return $user;
+    }
+}
