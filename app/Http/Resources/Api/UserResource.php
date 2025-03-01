e05ea4f8679faad31a20f290eae69987f215cc89
Guilherme Delmiro <guilhermedelmiro11@gmail.com>
2025-03-01 01:01:28 -0300

    refactor: removing toArray method from user resource

--- app/Http/Resources/Api/UserResource.php
+++ app/Http/Resources/Api/UserResource.php
@@ -2,29 +2,10 @@
 
 namespace App\Http\Resources\Api;
 
-use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
-use Illuminate\Support\Facades\Hash;
 
 class UserResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
-    public function toArray(Request $request): array
-    {
-        return [
-            'name' => $this->name,
-            'email' => $this->email,
-            'password' => Hash::make($this->password),
-            'telefone' => $this->telefone,
-            'is_admin' => $this->is_admin,
-            'is_valid' => $this->is_valid,
-        ];
-    }
-
     public static function collection($resource)
     {
         return tap(parent::collection($resource), function ($collection) use ($resource) {
