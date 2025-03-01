e0901bdfb20850406ecbeb264ba75eb0de05b998
Guilherme Delmiro <guilhermedelmiro11@gmail.com>
2025-02-28 23:27:48 -0300

    feat(user): add User model with passport token configuration

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,24 +9,15 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use OwenIt\Auditing\Contracts\Auditable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable implements Auditable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasApiTokens;
     use HasFactory;
     use Notifiable;
     use \OwenIt\Auditing\Auditable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
 
     /**
      * The attributes that should be hidden for serialization.
