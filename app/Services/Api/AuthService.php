4188795b6bbdfe3909919fabee76860270f0e107
Guilherme Delmiro <guilhermedelmiro11@gmail.com>
2025-03-01 01:15:02 -0300

    feat: implement auth service method to register and create token

---
+++ app/Services/Api/AuthService.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Services\Api;
+
+use App\Constants\AuthConstants;
+use App\DTO\Api\RegisterDTO;
+use App\Http\Resources\Api\UserResource;
+use App\Models\User;
+
+class AuthService
+{
+    public function __construct(
+        protected UserService $userService
+    ) {}
+
+    public function register(RegisterDTO $dto): User
+    {
+        $userData = (new UserResource($dto))->toArray(request());
+        return $this->userService->create($userData);
+    }
+
+    public function createUserToken(User $user): string
+    {
+        return $user->createToken(AuthConstants::TOKEN_NAME)->accessToken;
+    }
+}
