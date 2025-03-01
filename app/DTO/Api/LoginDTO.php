3b9b11aefb8c60fbcd39cede6908cec2a2517750
Guilherme Delmiro <guilhermedelmiro11@gmail.com>
2025-03-01 01:00:02 -0300

    feat: add data transfer objects for login and register

---
+++ app/DTO/Api/LoginDTO.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\DTO\Api;
+
+use App\Http\Requests\Api\Auth\LoginRequest;
+
+class LoginDTO
+{
+    public function __construct(
+        public readonly string $email,
+        public readonly string $password
+    ) {}
+
+    public static function fromRequest(LoginRequest $request): self
+    {
+        return new self(
+            email: $request->email,
+            password: $request->password
+        );
+    }
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(): array
+    {
+        return [
+            'email' => $this->email,
+            'password' => $this->password
+        ];
+    }
+}
---
+++ app/DTO/Api/RegisterDTO.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\DTO\Api;
+
+use App\Http\Requests\Api\User\StoreUserRequest;
+
+class RegisterDTO
+{
+    public function __construct(
+        public readonly string $name,
+        public readonly string $email,
+        public readonly string $password,
+        public readonly string $telefone,
+        public readonly bool $is_admin,
+        public readonly bool $is_valid
+
+    ) {}
+
+    public static function fromRequest(StoreUserRequest $request): self
+    {
+        return new self(
+            name: $request->name,
+            email: $request->email,
+            password: bcrypt($request->password),
+            telefone: $request->telefone,
+            is_admin: $request->is_admin,
+            is_valid: $request->is_valid,
+        );
+    }
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(): array
+    {
+        return [
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => $this->password,
+            'telefone' => $this->telefone,
+            'is_admin' => $this->is_admin,
+            'is_valid' => $this->is_valid,
+        ];
+    }
+}
