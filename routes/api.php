5857e03c115890de0269d5f4127f3d30c893159e
Guilherme Delmiro <guilhermedelmiro11@gmail.com>
2025-03-01 01:17:12 -0300

    feat: implement api routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,24 @@
 <?php
 
-use Illuminate\Http\Request;
+declare(strict_types = 1);
+
+use App\Http\Controllers\PassportAuthController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::middleware(['cors:api', 'json.response'])->group( function () {
+    Route::post('auth/register', [PassportAuthController::class, 'register']);
+    Route::post('auth/login', [PassportAuthController::class, 'login']);
+    Route::get('/', function () {
+        return "APmmI";
+    });
+    Route::middleware('auth:api')->group( function () 
+    {
+        Route::post('auth/logout', [PassportAuthController::class, 'logout']);
+
+        Route::resource('users', UserController::class);
+        Route::resource('posts', PostController::class);
+    });
+});
