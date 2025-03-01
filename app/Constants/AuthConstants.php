0f0a1034c0c479096dcdb6a297a7da845da4d475
Guilherme Delmiro <guilhermedelmiro11@gmail.com>
2025-03-01 19:15:41 -0300

    feat(auth): add new user-related constants messages

--- app/Constants/AuthConstants.php
+++ app/Constants/AuthConstants.php
@@ -14,5 +14,11 @@ class AuthConstants
         'invalid_credentials' => 'Credenciais inválidas. Verifique seu email e senha.',
         'logout_success' => 'Logout realizado com sucesso',
         'logout_failed' => 'Logout falhou',
+        'list_success' => 'Lista de usuários recuperada com sucesso',
+        'user_not_found' => 'Usuário não encontrado',
+        'user_found' => 'Usuário encontrado',
+        'user_updated' => 'Usuário atualizado com sucesso',
+        'user_updated_failed' => 'Usuário não pôde ser atualizado, tente novamente.',
+        'user_deleted' => 'Usuário deletado com sucesso',
     ];
 }
