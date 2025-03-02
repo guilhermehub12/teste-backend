fa055c11158b080a7ecf1887a35e810f00b64b9e
Guilherme Delmiro <guilhermedelmiro11@gmail.com>
2025-03-02 03:32:09 -0300

    refactor(auth): moving UserFactory to Api folder

--- database/factories/Api/UserFactory.php
+++ database/factories/Api/UserFactory.php
@@ -2,7 +2,7 @@
 
 declare(strict_types = 1);
 
-namespace Database\Factories;
+namespace Database\Factories\Api;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
